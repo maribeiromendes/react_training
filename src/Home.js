f3f801668a53e3fb25bd89a0f753d787d34d94b9
Mariana Ribeiro <33marian@gmail.com>
2021-04-21 19:11:12 -0300

    props e states

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mariana', id: 1},
         {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mariana', id: 2},
-        {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mariana', id: 3},
+        {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mari', id: 3},
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
+    useEffect(() => {
+    }, []);
 
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className='blog-preview' key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    
-
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mari') } title="Mari's blogs!"/>
         </div>
      );
 }
