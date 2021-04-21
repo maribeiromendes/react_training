d3a214b437d9f2b65506ee97ebe7871f32e51fdf
Mariana Ribeiro <33marian@gmail.com>
2021-04-20 22:21:50 -0300

    Adicionando blogposts

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
         <Navbar />
-        <Home />
+        <div className="content">
+          <Home />
+        </div>
     </div>
   );
 }
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,22 @@
+import { useState } from 'react'
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mariana', id: 1},
+        {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mariana', id: 2},
+        {title: 'My new website', body: 'lorem ipsum dolor sit amet...', author: 'mariana', id: 3},
+    ]);
+
     return ( 
-        <div className="content">
-            <h1>Home Page</h1>
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className='blog-preview' key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    
+
+                </div>
+            ))}
         </div>
      );
 }
